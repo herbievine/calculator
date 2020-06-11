dded79ad950b00f881b4231dd8f553fcc63a6b83
Herbie VINE <vineherbie@gmail.com>
2020-06-11 17:28:40 +0200

    Added Tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,65 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
+
 
 import 'package:calculator/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  group('String', () {
+    test('String.split() splits the string on the delimiter', () {
+      var string = 'foo,bar,baz';
+      expect(string.split(','), equals(['foo', 'bar', 'baz']));
+    });
+  
+    test('String.trim() removes surrounding whitespace', () {
+      var string = '  foo ';
+      expect(string.trim(), equals('foo'));
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    test('Testing string splitting and merging', () {
+      List<String> strings = [
+        'tHIS is SomE TeXT',
+        'More TEXT HERE',
+        'AND more here',
+        '23'
+      ];
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      List<String> expected = [
+        'This is some text',
+        'More text here',
+        'And more here',
+        '23'
+      ];
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      var counter = 0;
+
+      for (var i in strings) {
+        var firstLetter = i.toString().split('')[0].toUpperCase();;
+        var afterFirstLetter = i.toString().substring(1, i.length).toLowerCase();
+
+        var newString = '$firstLetter$afterFirstLetter';
+
+        expect(newString, equals(expected[counter]));
+        counter++;
+      }
+    });
+  });
+
+
+  group('List / Object', () {
+    test('Testing object in array', () {
+      List<Map<String, dynamic>> themes = [
+        {'red': 17},
+        {'blue': 32},
+        {'green': 25},
+        {'yellow': [92]}
+      ];
+  
+      expect(themes[0]['red'], equals(17));
+      expect(themes[2]['green'], equals(25));
+      expect(themes[3]['yellow'][0], equals(92));
+    });
   });
 }
